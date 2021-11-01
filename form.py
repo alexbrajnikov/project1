1500bc38bff493c30ac620ff122e4d6f259fca1d
alexbrajnikov <93008381+alexbrajnikov@users.noreply.github.com>
2021-11-01 12:43:51 -0400

    Update form.py

--- form.py
+++ form.py
@@ -9,7 +9,7 @@ driver.find_element_by_css_selector(input(id="exampleCheck1")).click()
 
 
 
-driver.find_element_by_css_selector(input(type='submit')).click()
+driver.find_element_by_xpath("input[@type='submit']").click()
 
 text=driver.find_element_by_class_name("alert-success").text()
 if text="Success! The Form has been submitted successfully!.":
