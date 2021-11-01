0f13b9d71646869dd3bfc60ab6dad48585d69de2
alexbrajnikov <93008381+alexbrajnikov@users.noreply.github.com>
2021-11-01 16:02:18 -0400

    Update form.py

--- form.py
+++ form.py
@@ -6,14 +6,12 @@ driver.find_element_by_name("name").send_keys("Alex")
 driver.find_element_by_name("email").send_keys("Alex@gmail.com")
 driver.find_element_by_xpath("//input[@id='exampleInputPassword1']").send_keys('1234')
 driver.find_element_by_css_selector(input(id="exampleCheck1")).click()
+dropdown = Select(driver.find_element(By.ID, "exampleFormControlSelect1")
+dropdown.select_by_index(0)
 
 
 
 driver.find_element_by_xpath("input[@type='submit']").click()
 
-text=driver.find_element_by_class_name("alert-success").text()
-if text="Success! The Form has been submitted successfully!.":
-    print("Pass")
-else:
-    print("Failed")
-    
+message = driver.find_element_by_class_name("alert-success").text()
+assert "success" in message
