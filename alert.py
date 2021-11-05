b7756752ad49255e611d29e83d005ee690bb4a43
alexbrajnikov <93008381+alexbrajnikov@users.noreply.github.com>
2021-11-05 11:51:10 -0400

    Update alert.py

--- alert.py
+++ alert.py
@@ -1,9 +1,20 @@
 from selenium import webdriver
 import time
 
+validatetext = "Option3"
 driver=webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
-driver.find_element(By.ID, "name").send_keys("Option3")
+driver.find_element(By.ID, "name").send_keys(validatetext)
 driver.find_element(By.ID, "alertbtn").click()
 alert = driver.switch_to.alert
-print(alert.text)
+if validatetext in alert.text:
+    assert
+    alert.accept()
+
+name = "Alex"
+driver.find_element(By.ID, "name").send_keys(name)
+driver.find_element_by_cssselector("#confirmbtn")
+alert = driver.switch_to.alert
+text = alert.text
+assert name in text
+alert.dissmiss()
