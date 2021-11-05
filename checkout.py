fc5ab3102bd6e1cdf4a78e963c9aa3c173e4414b
alexbrajnikov <93008381+alexbrajnikov@users.noreply.github.com>
2021-11-05 13:49:36 -0400

    Create checkout.py

---
+++ checkout.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+
+
+driver.get("https://rahulshettyacademy.com/seleniumPractise/")
+driver.find_element_by_xpath("// input[@type="search" ]").send_keys("ber")
+time.slep(5)
+products = driver.find_element_by_xpath("//div[@class='products']/div")
+count = len(products)
+assert count == 3
+buttons = find_elements_by_xpath("//div[@class='product-action']/button")
+for button in buttons:
+    button.click()
+    
+driver.find_elements_by_xpath("//a[@class='cart-icon']").click()
+driver.find_elements_by_xpath('//button[text()="PROCEED TO CHECKOUT"]').click()
+driver.find_element_by_cssselector("input.promoCode").send_keys('rahulshettyacademy')
