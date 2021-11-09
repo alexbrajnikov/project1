8bde244e2e7ad856790f3b5d7dcd89ea874331ff
alexbrajnikov <93008381+alexbrajnikov@users.noreply.github.com>
2021-11-09 16:02:37 -0500

    Update bank.py

--- bank.py
+++ bank.py
@@ -6,7 +6,7 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import Select
 
 driver=webdriver.Chrome()
-
+driver.implicitly_wait(20)
 select = Select(driver.find_element(By.XPATH, "//button[text()='Customer Login']"))
 select.select_by_value("2")
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
