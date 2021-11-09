5ea9fbb6699d9b00635af2530775973843152224
alexbrajnikov <93008381+alexbrajnikov@users.noreply.github.com>
2021-11-09 16:00:46 -0500

    Update swaglabs.py

--- swaglabs.py
+++ swaglabs.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import Select
 
+driver=webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
 driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
