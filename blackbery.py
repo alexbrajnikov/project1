2e25095dc4304b016688aeae9fd24739a1b62573
alexbrajnikov <93008381+alexbrajnikov@users.noreply.github.com>
2021-11-10 20:19:10 -0500

    Update blackbery.py

--- blackbery.py
+++ blackbery.py
@@ -1,31 +1,33 @@
-
 from selenium import webdriver
 import time
 
-#chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument("headless")
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("headless")
+from selenium.webdriver.common import by
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions, wait
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(20)
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 driver.find_element(By.LINK_TEXT, "Shop").click()
-products = driver.find_element(By.XPATH,"//div[@class='card h-100']")
+products = driver.find_elements(By.XPATH,"//div[@class='card h-100']")
 for product in products:
-   product_name = product.find_element(By.XPATH,"div/h4/a").text 
-   if product_name == "Blackberry":
-   product.find_element(By.XPATH,"//button").click
-   
-driver.find_element(By.XPATH,"//a[@class='nav-link btn btn-primary'").click()
-cart_element= find_element(by.XPATH,"//h4/a[@href='#']").text
-assert cart_element =="Blackberry"
+    product_name = product.find_element(By.XPATH, "div/h4/a").text
+    if product_name == "Blackberry":
+        product.find_element(By.XPATH, "div/button").click()
+
+driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
+cart_element = driver.find_element(By.XPATH, "//h4/a[@href='#']").text
+assert cart_element == "Blackberry"
 
 driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
 driver.find_element(By.CSS_SELECTOR, "input[id='country']").send_keys("uni")
-webDriverWait(driver,7)
-wait.until(expected_conditions.presense_of_element_located((By.LINK_TEXT,"United States of America")))
+
 driver.find_element(By.LINK_TEXT, "United States of America").click()
-driver.find_element(By.ID,"checkbox2" ).click()
+driver.find_element(By.CSS_SELECTOR,'div[class= "checkbox checkbox-primary"]').click()
 driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
 mss = driver.find_element(By.XPATH, "//*[@class='alert alert-success alert-dismissible']").text
-assert mss == "Success! Thank you! Your order will be delivered in next few weeks :-)."
-assert driver.find_element(By.ID,"checkbox2" ).is_selected()
+print(mss)
+assert  "Success! Thank you!" in mss
+assert driver.find_element(By.ID, "checkbox2").is_selected()
