c02b3b6fd877394ecbb50e0e1df99e19ab502ea5
alexbrajnikov <93008381+alexbrajnikov@users.noreply.github.com>
2021-11-10 19:23:48 -0500

    Update ActionChain.py

--- ActionChain.py
+++ ActionChain.py
@@ -1,17 +1,25 @@
 import driver as driver
 from selenium import webdriver
-from selenium.webdriver import ActionChains
+
 import time
+
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
 
 driver=webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
-action = ActionChain(driver)
-action.move_to_elent(driver.find_element(By.ID, "mousehover")).perform()
-action.move_to_elent(driver.find_element(By.LINK_TEXT, "Reload")).click().perform()
 
-'''
+
+
+
+
+action = ActionChains(driver)
+action.move_to_element(driver.find_element(By.ID, "mousehover")).perform()
+action.move_to_element(driver.find_element(By.LINK_TEXT, "Reload")).click().perform()
+
+
 driver.get("https://chercher.tech/practice/practice-pop-ups-selenium-webdriver")
 action.double_click(driver.find_element(By.ID, "double-click")).perform()
-alert = driver.switch_to.alert()
+alert = driver.switch_to.alert
 assert alert.text == "You double clicked me!!!, You got to be kidding me"
-alert.accept()
+alert.accept() 
